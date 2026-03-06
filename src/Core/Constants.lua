24dcf840da7aafaaa5e253ce7725524d10154411
NikitosZuev <komrovnikita@gmail.com>
2026-03-06 17:39:22 +0700

    Create Constants.lua

---
+++ src/Core/Constants.lua
@@ -0,0 +1,9 @@
+local Constants = {}
+
+Constants.MAX_PLACE_ATTEMPTS = 20
+Constants.MAX_UPGRADE_ATTEMPTS = 20
+Constants.MAX_SELL_ATTEMPTS = 20
+Constants.GAME_START_TIMEOUT = 180
+Constants.TOWER_FIND_RADIUS = 15
+
+return Constants
