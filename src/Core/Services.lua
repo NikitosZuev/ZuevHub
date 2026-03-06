e633b103f0a34b123ed01e43dcbf9213c69d6ecc
NikitosZuev <komrovnikita@gmail.com>
2026-03-06 17:38:39 +0700

    Create Services.lua

---
+++ src/Core/Services.lua
@@ -0,0 +1,13 @@
+local Services = {}
+
+Services.Players = game:GetService("Players")
+Services.HttpService = game:GetService("HttpService")
+Services.UserInputService = game:GetService("UserInputService")
+Services.ReplicatedStorage = game:GetService("ReplicatedStorage")
+Services.Workspace = game:GetService("Workspace")
+Services.VirtualUser = game:GetService("VirtualUser")
+Services.TeleportService = game:GetService("TeleportService")
+Services.TweenService = game:GetService("TweenService")
+Services.RunService = game:GetService("RunService")
+
+return Services
