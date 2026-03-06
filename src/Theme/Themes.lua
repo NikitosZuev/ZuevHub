e7093d3fed74983e1d933670966161b4508f9bdd
NikitosZuev <komrovnikita@gmail.com>
2026-03-06 17:41:17 +0700

    Create Themes.lua

---
+++ src/Theme/Themes.lua
@@ -0,0 +1,27 @@
+local Themes = {}
+
+Themes.Dark = {
+    BgMain = Color3.fromRGB(20,20,20),
+    BgCard = Color3.fromRGB(35,35,35),
+    Text = Color3.fromRGB(255,255,255)
+}
+
+Themes.Violet = {
+    BgMain = Color3.fromRGB(35,20,45),
+    BgCard = Color3.fromRGB(60,35,80),
+    Text = Color3.fromRGB(230,200,255)
+}
+
+Themes.Red = {
+    BgMain = Color3.fromRGB(45,15,15),
+    BgCard = Color3.fromRGB(70,25,25),
+    Text = Color3.fromRGB(255,200,200)
+}
+
+Themes.Blue = {
+    BgMain = Color3.fromRGB(15,20,45),
+    BgCard = Color3.fromRGB(30,40,70),
+    Text = Color3.fromRGB(200,220,255)
+}
+
+return Themes
